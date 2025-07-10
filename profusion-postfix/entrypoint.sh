220a5262abe7fdb00a84237ee854ffcc5ae000c5
max <max@errantminds.net>
2025-07-10 14:47:43 +0100

    fix entrypoint/dockerfile config template mismatch

--- profusion-postfix/entrypoint.sh
+++ profusion-postfix/entrypoint.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Substitute environment variables into the postfix config and put the final version in the postfix directory:
-envsubst < /opt/profusion-postfix/conf/main.cf.template > /etc/postfix/main.cf
+envsubst < /opt/profusion-postfix/main.cf.template > /etc/postfix/main.cf
 
 # Start postfix in foreground:
 exec postfix start-fg
