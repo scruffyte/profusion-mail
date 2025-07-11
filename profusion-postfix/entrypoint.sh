cbd103a0683a8a8dd8ecb3cc16c998c49dd515cd
max <max@errantminds.net>
2025-07-11 15:32:54 +0100

    it was dns

--- profusion-postfix/entrypoint.sh
+++ profusion-postfix/entrypoint.sh
@@ -3,6 +3,10 @@ set -e
 
 envsubst < /opt/profusion-postfix/main.cf.template > /etc/postfix/main.cf
 
+# it was DNS...
+mkdir -p /var/spool/postfix/etc
+cp /etc/resolv.conf /var/spool/postfix/etc/resolv.conf
+
 echo "Starting postfix..."
 # Start postfix in foreground:
 postfix start-fg
