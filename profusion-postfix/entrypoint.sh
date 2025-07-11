6c5b89da1efbfb6acfa59edd6106d6a3cd9858de
max <max@errantminds.net>
2025-07-11 15:11:50 +0100

    logging again

--- profusion-postfix/entrypoint.sh
+++ profusion-postfix/entrypoint.sh
@@ -3,10 +3,6 @@ set -e
 
 envsubst < /opt/profusion-postfix/main.cf.template > /etc/postfix/main.cf
 
-echo "Starting rsyslog..."
-# Start rsyslog to capture Postfix logs and redirect them to stdout
-rsyslogd
-
 echo "Starting postfix..."
 # Start postfix in foreground:
 postfix start-fg
